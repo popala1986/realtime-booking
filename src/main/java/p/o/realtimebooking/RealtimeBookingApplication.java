eb946a86fb0cc66dfece03fb51225000b3b7ee05
Paweł Opala <p.opala1986@gmail.com>
2025-09-07 20:39:26 +0200

    First Commit

---
+++ src/main/java/p/o/realtimebooking/RealtimeBookingApplication.java
@@ -0,0 +1,13 @@
+package p.o.realtimebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/p/o/realtimebooking/RealtimeBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package p.o.realtimebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
