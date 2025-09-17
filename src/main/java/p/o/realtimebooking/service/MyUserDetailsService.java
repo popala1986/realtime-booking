708c86ac76643967f496609fa72a656255f92a99
Paweł Opala <p.opala1986@gmail.com>
2025-09-17 13:27:43 +0200

    feat(service): add ReservationService, MyUserDetailsService

---
+++ src/main/java/p/o/realtimebooking/service/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package p.o.realtimebooking.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import p.o.realtimebooking.repository.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
--- src/main/java/p/o/realtimebooking/service/ReservationService.java
+++ src/main/java/p/o/realtimebooking/service/ReservationService.java
@@ -1,12 +1,18 @@
+
 package p.o.realtimebooking.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import p.o.realtimebooking.model.Seat;
-import p.o.realtimebooking.model.SeatStatus;
+import org.springframework.web.server.ResponseStatusException;
+import p.o.realtimebooking.model.*;
+import p.o.realtimebooking.repository.ReservationRepository;
 import p.o.realtimebooking.repository.SeatRepository;
+import p.o.realtimebooking.repository.UserRepository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,47 +20,67 @@ import java.util.List;
 public class ReservationService {
 
     private final SeatRepository seatRepository;
+    private final ReservationRepository reservationRepository;
+    private final UserRepository userRepository;
 
     public List<Seat> getAllSeats() {
         return seatRepository.findAll();
     }
 
     @Transactional
-    public Seat reserveSeat(Long seatId) {
-        Seat seat = seatRepository.findById(seatId)
-                .orElseThrow(() -> new RuntimeException("Seat not found!"));
+    public Reservation reserveSeat(Long seatId, String email) {
+        Seat seat = seatRepository.findByIdForUpdate(seatId);
+        if (seat == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Miejsce nie znalezione");
+        }
 
         if (seat.getStatus() != SeatStatus.AVAILABLE) {
-            throw new RuntimeException("Seat is already booked!");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Miejsce już zarezerwowane");
         }
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Użytkownik nie znaleziony"));
 
         seat.setStatus(SeatStatus.RESERVED);
-        return seatRepository.save(seat);
+        seatRepository.save(seat);
+
+        Reservation reservation = new Reservation();
+        reservation.setSeat(seat);
+        reservation.setUser(user);
+        reservation.setReservationTime(LocalDateTime.now());
+        reservation.setExpiresAt(LocalDateTime.now().plusMinutes(1));
+
+        return reservationRepository.save(reservation);
     }
 
     @Transactional
-    public Seat releaseSeat(Long seatId) {
-        Seat seat = seatRepository.findById(seatId)
-                .orElseThrow(() -> new RuntimeException("Seat not found!"));
+    public List<Seat> removeExpiredReservations() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Reservation> expiredReservations = reservationRepository.findAllByExpiresAtBefore(now);
+        List<Seat> freedSeats = new ArrayList<>();
 
-        if (seat.getStatus() == SeatStatus.AVAILABLE) {
-            throw new RuntimeException("Seat is already available!");
+        for (Reservation reservation : expiredReservations) {
+            Seat seat = reservation.getSeat();
+            seat.setStatus(SeatStatus.AVAILABLE);
+            seatRepository.save(seat);
+            reservationRepository.delete(reservation);
+            freedSeats.add(seat);
         }
+        System.out.println("Sprawdzono i usunięto wygasłe rezerwacje o: " + now);
+        return freedSeats;
+    }
 
+    @Transactional
+    public Reservation cancelReservation(Long seatId, String email) {
+        Reservation reservation = reservationRepository.findBySeatIdAndUserEmail(seatId, email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Brak rezerwacji do anulowania."));
+
+        Seat seat = reservation.getSeat();
         seat.setStatus(SeatStatus.AVAILABLE);
-        return seatRepository.save(seat);
-    }
+        seatRepository.save(seat);
 
-    @Transactional
-    public Seat occupySeat(Long seatId) {
-        Seat seat = seatRepository.findById(seatId)
-                .orElseThrow(() -> new RuntimeException("Seat not found!"));
+        reservationRepository.delete(reservation);
 
-        if (seat.getStatus() != SeatStatus.RESERVED) {
-            throw new RuntimeException("Seat must be reserved before occupying!");
-        }
-
-        seat.setStatus(SeatStatus.OCCUPIED);
-        return seatRepository.save(seat);
+        return reservation;
     }
 }
